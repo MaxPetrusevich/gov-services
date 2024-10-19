2225f0984b99e5c7eba7ed7612b24d2c32b960ea
MaxPetrusevich <lomic2004@yandex.ru>
2024-10-19 12:54:49 +0300

    Добавлена система миграций баз данных, создана бд при помощи Liquibase

---
+++ src/main/kotlin/by/course/govservices/GovServicesApplication.kt
@@ -0,0 +1,11 @@
+package by.course.govservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GovServicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<GovServicesApplication>(*args)
+}
---
+++ src/test/kotlin/by/course/govservices/GovServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.course.govservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GovServicesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
